72c877ffb9a943b9d223d3ac2f4e768c31292549
Chris Del <drumgod101@gmail.com>
2021-08-26 12:09:26 -0600

    Basic nav working

---
+++ src/components/screens/Profile.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import * as React from 'react'
+import {View, Text} from 'react-native'
+
+const Profile: () => React$Node = ({navigation}) => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  )
+}
+
+export default Profile
